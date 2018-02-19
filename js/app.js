0a30dd8e4ebfa9e287e53fbc7e811c3608015109
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-02-19 16:08:37 -0500

    added jQuery, app.js with modular design

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Treehouse Tech Project 4: Tic Tac Toe
+
+// Ronald van der Bergh
+
+// I aim for extra credit with this project.
+
+// main app module, to keep names from cluttering global namespace
+! function() {
+
+}();
