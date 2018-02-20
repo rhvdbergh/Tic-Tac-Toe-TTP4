bb6cb1a1f2e820acc145973a0067e2a0518d08a4
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-02-20 15:57:59 -0500

    testForFinish refactored + now tests for draw

--- js/app.js
+++ js/app.js
@@ -60,40 +60,44 @@
     // 1 = player 1, 2 = player 2
     function testForFinish() {
 
+        // tests a given combination of 3 co-ordinates in the 2d array to see if they all contain p(layer)
+        function testCombination(r1, c1, r2, c2, r3, c3, p) {
+            if (boardArray[r1][c1] === p && boardArray[r2][c2] === p && boardArray[r3][c3] === p) {
+                return true;
+            }
+            return false;
+        }
         // player 1 = o, player 2 = x
         // first, we test all player 1's tokens to see if three is in a line
-        // then, if the test has not returned anything, test for player 2
+        // then, if the test has not returned anything, test for player 2 - hence the for loop
         // finally, check if there is a draw
         for (let player = 1; player < 3; player++) {
             // test for all possible combinations of player tokens that can possible line up in 3
-            if (boardArray[0][0] === player && boardArray[0][1] === player && boardArray[0][2] === player) {
-                return player;
-            }
-            if (boardArray[0][0] === player && boardArray[1][0] === player && boardArray[2][0] === player) {
-                return player;
-            }
-            if (boardArray[0][0] === player && boardArray[1][1] === player && boardArray[2][2] === player) {
-                return player;
-            }
+            if (testCombination(0, 0, 0, 1, 0, 2, player)) return player;
+            if (testCombination(0, 0, 1, 0, 2, 0, player)) return player;
+            if (testCombination(0, 0, 1, 1, 2, 2, player)) return player;
 
-            if (boardArray[0][1] === player && boardArray[1][1] === player && boardArray[1][2] === player) {
-                return player;
-            }
+            if (testCombination(0, 1, 1, 1, 1, 2, player)) return player;
 
-            if (boardArray[0][2] === player && boardArray[1][1] === player && boardArray[2][0] === player) {
-                return player;
-            }
-            if (boardArray[0][2] === player && boardArray[1][2] === player && boardArray[2][2] === player) {
-                return player;
-            }
+            if (testCombination(0, 2, 1, 1, 2, 0, player)) return player;
+            if (testCombination(0, 2, 1, 2, 2, 2, player)) return player;
 
-            if (boardArray[1][0] === player && boardArray[1][1] === player && boardArray[1][2] === player) {
-                return player;
-            }
+            if (testCombination(1, 0, 1, 1, 1, 2, player)) return player;
 
-            if (boardArray[2][0] === player && boardArray[2][1] === player && boardArray[2][2] === player) {
-                return player;
+            if (testCombination(2, 0, 2, 1, 2, 2, player)) return player;
+        }
+
+        // test for a draw
+        // a draw is when all the boxes have tokens on them but no one won
+        // so nine tokens have been placed
+        let count = 0;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (boardArray[i][j] > 0) count++;
             }
+        }
+        if (count === 9) {
+            return 0;
         }
 
         // if the test is at this point, the game must still be in progress
