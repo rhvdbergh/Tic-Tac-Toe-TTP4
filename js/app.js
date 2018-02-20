4146bcd6dacad3576c22ee26a6bf4837480b380d
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-02-20 01:05:10 -0500

    hover over boxes produce player's token - classes added

--- js/app.js
+++ js/app.js
@@ -7,13 +7,19 @@
 // main app module, to keep names from cluttering global namespace
 ! function() {
 
-
     const $startScreen = $('#start');
     const $boardScreen = $('#board');
     const $finishScreen = $('#finish');
     const $player1 = $('#player1');
     const $player2 = $('#player2');
     const $boxes = $('.boxes');
+    // boardArray emulates the board in a three by three pattern
+    // using 3 nested arrays
+    let boardArray = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
 
     let activePlayer = 1; // player 1 is active; value alternates between 1 and 2
 
@@ -23,6 +29,7 @@
         $finishScreen.hide();
         $boardScreen.show();
         $player1.addClass('active');
+        $('.box').addClass('p1');
     }
 
     // toggles player between 1 and 2
@@ -31,12 +38,28 @@
             activePlayer = 2;
             $player1.removeClass('active');
             $player2.addClass('active');
+            $('.box').each(function(i, box) {
+                if (!($(box).hasClass('clicked'))) {
+                    $(box).removeClass('p1').addClass('p2');
+                }
+            });
         } else {
             activePlayer = 1;
             $player2.removeClass('active');
             $player1.addClass('active');
-
+            $('.box').each(function(i, box) {
+                if (!($(box).hasClass('clicked'))) {
+                    $(box).removeClass('p2').addClass('p1');
+                }
+            });
         }
+    }
+
+    // tests if the game is over
+    // returns the winner; 0 = draw, 1 = player 1, 2 = player 2
+    function testForFinish() {
+
+        return 0;
     }
 
     // INITIAL SETUP
@@ -57,14 +80,14 @@
         }
     });
 
-    // event handler for game board boxes
-    $boxes.hover((event) => { console.log(event.target); }, (event) => {});
+    // $boxes.hover((event) => { console.log(event.target); }, (event) => {});
 
+    // event handler for game board boxes    
     $boxes.on('click', (event) => {
         const box = event.target;
 
         if (!($(box).hasClass('clicked'))) {
-            console.log('clickin');
+            console.log(box.id);
             if (activePlayer === 1) {
                 $(box).addClass('box-filled-1');
             } else {
@@ -75,7 +98,6 @@
 
             togglePlayer();
         };
-
     });
 
 }();
