55839704f6e269be74a41f551a9f59bc90b0e02a
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-02-19 22:54:24 -0500

    player alternates, on click symbol appears, can't click on square twice

--- js/app.js
+++ js/app.js
@@ -9,14 +9,26 @@
 
 
     const $startScreen = $('#start');
-    const $board = $('#board');
+    const $boardScreen = $('#board');
     const $finishScreen = $('#finish');
+    const $player1 = $('#player1');
+    const $player2 = $('#player2');
+    const $boxes = $('.boxes');
+
+    let activePlayer = 1; // player 1 is active; value alternates between 1 and 2
 
     // draws game board - hides start screen and win screens
     function drawBoard() {
         $startScreen.hide();
         $finishScreen.hide();
-        $board.show();
+        $boardScreen.show();
+        $player1.addClass('active');
+    }
+
+    // toggles player between 1 and 2
+    function togglePlayer() {
+        if (activePlayer === 1) activePlayer = 2
+        else activePlayer = 1;
     }
 
     // INITIAL SETUP
@@ -35,6 +47,27 @@
             const button = event.target;
             drawBoard();
         }
+    });
+
+    // event handler for game board boxes
+    $boxes.hover((event) => { console.log(event.target); }, (event) => {});
+
+    $boxes.on('click', (event) => {
+        const box = event.target;
+
+        if (!($(box).hasClass('clicked'))) {
+            console.log('clickin');
+            if (activePlayer === 1) {
+                $(box).addClass('box-filled-1');
+            } else {
+                $(box).addClass('box-filled-2');
+            }
+
+            $(box).addClass('clicked');
+
+            togglePlayer();
+        };
 
     });
+
 }();
