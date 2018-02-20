5ded376ab222565f64549d0e0ccc9bcff805fced
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-02-19 20:47:02 -0500

    start screen shows at startup, javascript disabled warning

--- js/app.js
+++ js/app.js
@@ -6,5 +6,20 @@
 
 // main app module, to keep names from cluttering global namespace
 ! function() {
+    // draws game board - hides start screen and win screens
+
+    // remove the JavaScript disabled message
+    $('#js_disabled').hide();
+
+    $('#start').show();
+
+
+    function drawBoard() {
+        $('#start').hide();
+        $('#finish').hide();
+        $('#board').show();
+    }
+
+    // drawBoard();
 
 }();
